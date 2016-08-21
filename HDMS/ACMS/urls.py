25832f06058a2219c4807ba3d00db9d3fb83e4fe
koyexes <koyagabriel@gmail.com>
2016-08-21 11:03:10 +0100

    Perfected the login logic

--- HDMS/ACMS/urls.py
+++ HDMS/ACMS/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "ACMS"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.index, name = 'login_index'),
     url(r'^homepage/$', views.homepage, name='homepage'),
 
 
