f57220ef623a5c3b0966ab06bf1f0017fecea82e
koyexes <koyagabriel@gmail.com>
2016-08-23 13:01:50 +0100

    Authentication works properly configured the authentication system to return the appropriate message if the user is inactive or doesn't exist

--- HDMS/ACMS/views.py
+++ HDMS/ACMS/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseRedirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import hashers
+
 
 # Create your views here.
 
@@ -12,17 +15,29 @@ def index(request):
     if request.method == "POST": # checking if it's a post request
         form = LoginForm(request.POST)
         if form.is_valid():
-            user = authenticate(username = request.POST['username'], password = request.POST['password'])
+            user = authenticate(username = form.cleaned_data['username'], password = form.cleaned_data['password'])
             if user is not None:
                 if user.is_active: # checking if the user is still active
                     login(request, user) # logging the user in
                     request.session.set_expiry(1800) # setting the session expiry time, after which login is required
                     return HttpResponseRedirect(reverse('ACMS:homepage'))
+
             else:
-                messages.error(request, 'Incorrect username and password!!!') # saving message error to be displayed on template
-                return HttpResponseRedirect(reverse('ACMS:index'))
+                try:
+                    username = User.objects.get(username = form.cleaned_data["username"])
+                    # since authenticate method returns none for inactive registered users, the if statement below takes care of checking if user exist
+                    # but inactive in order to forward the right feedback message
+                    if username and hashers.check_password(form.cleaned_data["password"], User.objects.get(username = username).password):
+                        messages.error(request, "Your account has been disabled! Contact Admin...")
+                        return HttpResponseRedirect(reverse('ACMS:index'))
+                    else:
+                         messages.error(request, 'Incorrect username and password!!!') # saving message error to be displayed on template
+                         return HttpResponseRedirect(reverse('ACMS:index'))
+                except:
+                    messages.error(request, 'Incorrect username and password!!!') # saving message error to be displayed on template
+                    return HttpResponseRedirect(reverse('ACMS:index'))
 
-
+# if the request is a GET request
     else:
         form = LoginForm() # creating new form
         if request.path == "/acms/login/":
@@ -31,10 +46,14 @@ def index(request):
 
     return render(request, 'acms/index.html', {"form": form})
 
+
+
 @login_required(redirect_field_name="", login_url='/acms/login') # login required to view this page
 def homepage(request):
-    context = {"name" : "%s %s" % (request.user.last_name, request.user.first_name), "username" : request.user.username}
-    return render(request, 'acms/homepage.html', context)
+    admin = False
+    if request.user.is_staff: admin = True # checking if the user has administrative rights
+    context = {"name" : "%s %s" % (request.user.last_name, request.user.first_name), "username" : request.user.username, "admin" : admin} # declaring the template context
+    return render(request, 'acms/homepage.html', context) # rendering the homepage template
 
 
 
