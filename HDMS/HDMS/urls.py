174e6bea14156ff9b4a5d908bdf6ce75cf1448eb
koyexes <koyagabriel@gmail.com>
2016-08-24 02:40:10 +0100

    Database models changes changed the models inorder to accomodate imports from mysql dump file for patient, hmo and drug tables

--- HDMS/HDMS/urls.py
+++ HDMS/HDMS/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+admin.site.site_header = "HDMS Administration"
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^acms/', include('ACMS.urls') )
